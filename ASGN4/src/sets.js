07b71e7686991be117397997ae066009a91b05c0
jingxiye <146494416+jingxiye@users.noreply.github.com>
2024-05-25 19:10:51 -0700

    Add files via upload

---
+++ ASGN4/src/sets.js
@@ -0,0 +1,10 @@
+let g_legsAngle = 0;
+let g_secondJointAngle = 0;
+let g_tailAngleX = 15;
+let g_tailAngleY = 0;
+let x_val = 0;
+let y_val = 0;
+let g_headTransform = 0;
+let g_bodyTransform = 0;
+let g_bodyRotate = 0;
+
